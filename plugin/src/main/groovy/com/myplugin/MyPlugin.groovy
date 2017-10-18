0ee3babef6d0a8628008f27026cc649f3257d82e
lihong <lihong_sail@163.com>
2017-10-18 16:23:29 +0800

    study

--- plugin/src/main/groovy/com/myplugin/MyPlugin.groovy
+++ plugin/src/main/groovy/com/myplugin/MyPlugin.groovy
@@ -19,12 +19,12 @@ public class MyPlugin implements Plugin<Project> {
         System.out.println("************${project.testExt.modelName}************")
         System.out.println("************${project.extensions.findByName("testExt") == null}************")
         System.out.println("************${project.extensions.findByName("testExt").hasProperty("modelName")}************")
-        System.out.println("************${project.extensions.findByName("testExt").getProperties().get("modelName")}************")
-        System.out.println("************${project.extensions.getByName("testExt").modelName}************")
 
         def android = project.extensions.getByType(AppExtension)
         if (project.plugins.hasPlugin(AppPlugin)) {
             android.applicationVariants.all { variant ->
+                System.out.println("************${project.extensions.findByName("testExt").getProperties().get("modelName")}************")
+                System.out.println("************${project.extensions.getByName("testExt").modelName}************")
                 def name = variant.name
                 def versionName = variant.versionName
                 def versionCode = variant.versionCode
